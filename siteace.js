4f89b88ac370ea8bf3dba8badcbfe33f354d1cf4
Guilherme Guerra <guerrinha@comum.org>
2016-02-09 23:21:30 -0200

    Add login support

--- siteace.js
+++ siteace.js
@@ -2,6 +2,11 @@ Websites = new Mongo.Collection("websites");
 
 if (Meteor.isClient) {
 
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+
   /////
   // template helpers
   /////
